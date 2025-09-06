396112d145d92ef232ec06f64220a5ffdf7763ff
Rob Terpilowski <robt@lynden.com>
2025-09-06 11:56:21 -0700

    Remove logging of broker status updates for cleaner output

--- implementations/broker-api/paper-broker-api-impl/src/main/java/com/sumzerotrading/broker/paper/PaperBroker.java
+++ implementations/broker-api/paper-broker-api-impl/src/main/java/com/sumzerotrading/broker/paper/PaperBroker.java
@@ -504,7 +504,8 @@ public class PaperBroker implements IBroker, Level1QuoteListener {
             brokerStatus.setFundingAccruedOrPaid(fundingAccruedOrPaid); // Update total funding paid or collected
             brokerStatus.setFundingRateAnnualized(((fundingRate / 8.0) * 24.0 * 365.0) * 100.0);
 
-            logger.info("Broker status updated: {}", brokerStatus); // Log the updated status
+            // logger.info("Broker status updated: {}", brokerStatus); // Log the updated
+            // status
         }
     }
 
